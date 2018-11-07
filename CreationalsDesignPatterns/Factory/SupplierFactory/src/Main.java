69678af25f7a48eb1b9f52d5e591238cf08312b8
Leandro Maro <leandro.maro@globant.com>
2018-11-07 18:32:39 -0300

    Factory Example using suppliers

---
+++ CreationalsDesignPatterns/Factory/SupplierFactory/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Supplier<ShapeFactory> shapeFactory =  ShapeFactory::new;
+        //call draw method of circle
+        shapeFactory.get().getShape("circle").draw();
+        //call draw method of Rectangle
+        shapeFactory.get().getShape("rectangle").draw();
+    }
+}
---
+++ CreationalsDesignPatterns/Factory/SupplierFactory/src/ShapeFactory.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class ShapeFactory {
+    final static Map<String, Supplier<Shape>> map = new HashMap<>();
+    static {
+        map.put("CIRCLE", Circle::new);
+        map.put("RECTANGLE", Rectangle::new);
+    }
+    public Shape getShape(String shapeType){
+        Supplier<Shape> shape = map.get(shapeType.toUpperCase());
+        if(shape != null) {
+            return shape.get();
+        }
+        throw new IllegalArgumentException("No such shape " + shapeType.toUpperCase());
+    }
+}
