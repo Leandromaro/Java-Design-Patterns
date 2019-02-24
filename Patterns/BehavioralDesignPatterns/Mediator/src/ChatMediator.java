476a2dff1ba09af9aed814895b0bc2cc8dfb30ce
Leandro Maro <leandro.maro@globant.com>
2019-02-24 19:49:54 -0300

    Adding Mediator

---
+++ Patterns/BehavioralDesignPatterns/Mediator/src/ChatMediator.java
@@ -0,0 +1,8 @@
+package BehavioralDesignPatterns.Mediator.src;
+
+public interface ChatMediator {
+
+    public void sendMessage(String msg, User user);
+
+    void addUser(User user);
+}
---
+++ Patterns/BehavioralDesignPatterns/Mediator/src/UserImpl.java
@@ -0,0 +1,19 @@
+package BehavioralDesignPatterns.Mediator.src;
+
+public class UserImpl extends User {
+    public UserImpl(ChatMediator med, String name) {
+        super(med, name);
+    }
+
+    @Override
+    public void send(String msg){
+        System.out.println(this.name+": Sending Message="+msg);
+        // the user instead of sending the message individually uses the mediator
+        // so it's loosely coupled to other users
+        mediator.sendMessage(msg, this);
+    }
+    @Override
+    public void receive(String msg) {
+        System.out.println(this.name+": Received Message:"+msg);
+    }
+}
