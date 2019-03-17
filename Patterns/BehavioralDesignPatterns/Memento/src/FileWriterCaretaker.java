3f23657f8cc3b8ab452410f0dd5d81ba94c8e38c
Leandro Maro <leandro.maro@globant.com>
2019-03-17 19:52:19 -0300

    Adding memento pattern

---
+++ Patterns/BehavioralDesignPatterns/Memento/src/FileWriterCaretaker.java
@@ -0,0 +1,14 @@
+package BehavioralDesignPatterns.Memento.src;
+
+public class FileWriterCaretaker {
+
+    private Object obj;
+
+    public void save(FileWriterUtil fileWriter){
+        this.obj=fileWriter.save();
+    }
+
+    public void undo(FileWriterUtil fileWriter){
+        fileWriter.undoToLastSave(obj);
+    }
+}
